bd2833a847861758dfb442e2757f368026e00abf
piyushnh <piyush.n.h@gmail.com>
2020-10-02 23:50:31 +0530

    commented debugger attach code from manage.py

--- manage.py
+++ manage.py
@@ -12,21 +12,21 @@ if __name__ == '__main__':
     from django.conf import settings
 
 
-    if settings.DEBUG:
-        # import socket
-        # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # sock.close()
+    # if settings.DEBUG:
+    #     # import socket
+    #     # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #     # sock.close()
 
 
 
-        if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN') :
-            import ptvsd
+    #     if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN') :
+    #         import ptvsd
 
-            try:
-                ptvsd.enable_attach(address=('localhost', 7913))
-                print('Debugger Attached!')
-            except OSError:
-                print("ptvsd port already in use.")
+    #         try:
+    #             ptvsd.enable_attach(address=('localhost', 7913))
+    #             print('Debugger Attached!')
+    #         except OSError:
+    #             print("ptvsd port already in use.")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
